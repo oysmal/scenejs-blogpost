1fffd3e7af51a148b864b4aea17c761b2dad036e
Øystein Malt <me@oysmal.com>
2021-01-11 23:19:40 +0100

    Testing out some animations

--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
 import Scene from "scenejs";
 import "./truck.css";
+import "./main.css";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const scene = new Scene({
-    ".truck": {
-      0: {
-        transform: "translateX(0px) translateZ(0)",
+  const truck = document.querySelector(".truck");
+  const scene = new Scene(
+    {
+      ".truck": {
+        0: {
+          transform: "translateX(0px) translateZ(0)",
+        },
+        1: {
+          transform: "translateX(0px) translateZ(0)",
+        },
+        2: {
+          transform: `translateX(${-window.innerWidth * 1.25}) translateZ(0)`,
+        },
       },
-      1: {
-        transform: `translateX(${-window.innerWidth * 1.25}) translateZ(0)`,
-      },
+      ".items .item": (i) => ({
+        0: {
+          transform: `translateX(0px) translateY(0px) scale(1.0)`,
+        },
+        1: {
+          transform: `translateX(${
+            truck.getBoundingClientRect().x
+          }px) translateY(${truck.getBoundingClientRect().y}px) scale(0.2)`,
+        },
+        options: {
+          delay: 0.5 * i,
+        },
+      }),
     },
-  });
-  scene.setSelector(".truck");
-  scene.setDuration(10);
+    {
+      selector: true,
+      duration: 10,
+    }
+  );
   scene.play();
   console.log(scene);
   setTimeout(() => console.log(scene), 200);
+
+  document.getElementById("toggle").addEventListener("click", () => {
+    switch (scene.getPlayState()) {
+      case "running":
+        scene.pause();
+        break;
+      case "paused":
+        scene.play();
+        break;
+    }
+  });
 });
