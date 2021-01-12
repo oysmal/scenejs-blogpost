cff97e5ce3beadf9e7c4a9fc596f7a5b9bc63d0e
Øystein Malt <me@oysmal.com>
2021-01-12 17:46:47 +0100

    Completing rough animation. Might need some more tweaking

--- src/index.js
+++ src/index.js
@@ -2,43 +2,88 @@ import Scene from "scenejs";
 import "./truck.css";
 import "./main.css";
 
+function getDiffRect(rect1, rect2) {
+  return {
+    x: rect1.x - rect2.x,
+    y: rect1.y - rect2.y,
+  };
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const truck = document.querySelector(".truck");
+  const truckBounds = truck.getBoundingClientRect();
+  const items = document.querySelectorAll(".items > .item");
   const scene = new Scene(
     {
       ".truck": {
         0: {
           transform: "translateX(0px) translateZ(0)",
         },
-        1: {
+        4: {
           transform: "translateX(0px) translateZ(0)",
         },
-        2: {
+        6: {
           transform: `translateX(${-window.innerWidth * 1.25}) translateZ(0)`,
         },
       },
-      ".items .item": (i) => ({
+      ".backdoor": {
         0: {
-          transform: `translateX(0px) translateY(0px) scale(1.0)`,
+          transform: `rotateZ(100deg)`,
         },
-        1: {
-          transform: `translateX(${
-            truck.getBoundingClientRect().x
-          }px) translateY(${truck.getBoundingClientRect().y}px) scale(0.2)`,
+        3: {
+          transform: `rotateZ(100deg)`,
         },
-        options: {
-          delay: 0.5 * i,
+        4: {
+          transform: `rotateZ(0deg)`,
         },
-      }),
+      },
+      ".items .item": (i) => {
+        const itemBounds = items.item(i).getBoundingClientRect();
+        const x1 =
+          getDiffRect(truckBounds, itemBounds).x + itemBounds.width * 0.2;
+        const y1 =
+          getDiffRect(truckBounds, itemBounds).y -
+          itemBounds.height +
+          truckBounds.height;
+        const x2 = x1 - truckBounds.width / 2;
+        const y2 = y1 - truckBounds.height / 3;
+        console.log(y2);
+
+        return {
+          0: {
+            transform: `translateX(0px) translateY(0px) scaleY(1.0) scaleX(1.0)`,
+            opacity: 1.0,
+          },
+          0.5: {
+            transform: `translateX(${x1}px) translateY(${y1}px) scaleY(0.4) scaleX(${
+              (itemBounds.height / itemBounds.width) * 0.4
+            })`,
+          },
+          1: {
+            transform: `translateX(${x2}px) translateY(${y2}px)`,
+            opacity: 1.0,
+          },
+          1.1: {
+            opacity: 0.0,
+          },
+          options: {
+            delay: 1 * i,
+          },
+        };
+      },
     },
     {
       selector: true,
-      duration: 10,
+      duration: 15,
     }
   );
-  scene.play();
-  console.log(scene);
-  setTimeout(() => console.log(scene), 200);
+
+  scene.on("ended", () => {
+    const modal = document.querySelector(".modal");
+    const backdrop = document.querySelector(".backdrop");
+    modal.classList.toggle("visible");
+    backdrop.classList.toggle("visible");
+  });
 
   document.getElementById("toggle").addEventListener("click", () => {
     switch (scene.getPlayState()) {
